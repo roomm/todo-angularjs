4c5e36c1b9a8d457fba065b3e281aa06ffd8c694
Roman Boix <rboix90@gmail.com>
2016-07-04 20:25:05 +0200

    Added CRD
    Create, Read, Delete

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -2,4 +2,27 @@ angular.module('todoAjsApp')
   .controller('mainController', function ($scope, $http) {
     var vm = this;
 
+    vm.todos = [{"name": "TODO1", "done": false}, {"name": "TODO2", "done": false}, {"name": "TODO3", "done": false}]
+
+    vm.newTodo = newTodo;
+    vm.finishTodo = finishTodo;
+    vm.deleteTodo = deleteTodo;
+
+    function newTodo() {
+      var txt = vm.new_todo;
+      vm.todos.push({"name": txt, "done": false});
+      vm.new_todo = undefined;
+    }
+
+    function finishTodo(todo) {
+      console.log("fonoshed", todo);
+      if (todo !== undefined) {
+        todo.done = true;
+      }
+    }
+
+    function deleteTodo(todo) {
+      var index = vm.todos.indexOf(todo);
+      vm.todos.splice(index, 1);
+    }
   });
